393a30e1494917eb5d931638dc3fae806aae7d58
mac <tchikh3@gmail.com>
2021-04-28 01:33:16 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { ZBar } from '@ionic-native/zbar/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { CallNumber } from '@ionic-native/call-number/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -11,7 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [ZBar,CallNumber,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-
+import { ZBar, ZBarOptions } from '@ionic-native/zbar/ngx';
+import { CallNumber } from '@ionic-native/call-number/ngx';
+  
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+selector: 'app-home', templateUrl: 'home.page.html', styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+zbarOptions:any; scannedResult:any;
+constructor(
+private zbar: ZBar, private callNumber: CallNumber
+){
+this.zbarOptions = { flash: 'off', drawSight: false
+}
+}
+scanQR(){ this.zbar.scan(this.zbarOptions)
+    .then(result => { console.log(result); this.scannedResult = result;
+    })
+    .catch(error => {
+    alert(error);
+     
+    }); }
+call(number) {
+    this.callNumber.callNumber(number, true)
+    .then(res => console.log('Launched dialer!', res))
+    .catch(err => console.log('Error launching dialer', err));
+    }
 
 }
